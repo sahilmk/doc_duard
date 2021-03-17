fb6446b1629ae9548ef2eea124e15d56e37dbf7a
sahilmk <sahilkanjariya00@gmail.com>
2021-03-17 21:29:53 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:doc_guard/screens/wrapper.dart';
+import 'package:flutter/material.dart';
+
+void main()=> runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: wrapper(),
+    );
+  }
+}
+
+
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:doc_guard/screens/authen/authen.dart';
+import 'package:doc_guard/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return myApp();
+  }
+}
